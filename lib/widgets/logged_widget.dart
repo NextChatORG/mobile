7196a5629804fb43924b1887a0d539c7879c2073
Daniel Solarte <danielsolartech@hotmail.com>
2021-03-15 12:44:28 -0500

    fix: LoggedWidget scroll view

--- lib/widgets/logged_widget.dart
+++ lib/widgets/logged_widget.dart
@@ -1,47 +1,44 @@
 import 'package:flutter/material.dart';
 
 class LoggedWidget extends StatelessWidget {
-  LoggedWidget({Key key, this.children}) : super(key: key);
   final List<Widget> children;
+
+  LoggedWidget({Key key, this.children}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "NextChat",
-                      style: TextStyle(
-                        color: Colors.indigo,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.search_rounded,
-                        color: Colors.indigo[200],
-                        size: 24.0,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        shrinkWrap: true,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "NextChat",
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              ...this.children,
-            ],
+                IconButton(
+                  icon: Icon(
+                    Icons.search_rounded,
+                    color: Colors.indigo[200],
+                    size: 24.0,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
-        ),
+          ...this.children,
+        ],
       ),
     );
   }
